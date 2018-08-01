6e515b27b1ecbc141adf7c944910b9b34c844eeb
Malric Philippe <malricp@majsrv1.iric.ca>
2018-08-01 14:20:09 -0400

    apiLocalColectionRNA

--- api/api_local.js
+++ api/api_local.js
@@ -38,20 +38,20 @@ app.get("/test", function (req, res) {
 })
 
 
-app.get("/ncm_grouped_Low_std_dev/collection=:collections/kip=:skip/limit=:limit/countMin=:cmin/stdDevMax=:stdDevMax", function (req, res) {
+app.get("/ncm_grouped_Low_std_dev_so/collection=:collections/skip=:skip/limit=:limit/countMin=:cmin/stdDevMax=:stdDevMax", function (req, res) {
   //console.log("id :" + req.params.id)
 
-  db.collection(req.params.collections).aggregate([{ $project: { nts: 1 } },
+  db.collection(req.params.collections).aggregate([{ $project: { nts: 1, info:{root:1} } },
   { $unwind: { path: "$nts" } },
-  { $project: { nts: { $arrayElemAt: ["$nts.ncmTabDG_so", 0] }, score: "$nts.score", rna: "$nts.rna_id",pos: "$nts.position" } },
-  { $project: { ncm: "$nts.ncm.merge", score: 1,rna:1,pos:1} },
+  { $project: { nts: { $arrayElemAt: ["$nts.ncmTabDG_so", 0] }, score: "$nts.score", rna: "$nts.rna_id",pos: "$nts.position",exp:"$info.root" } },
+  { $project: { ncm: "$nts.ncm.merge", score: 1,rna:1,pos:1,exp:1} },
   {
     $group: {
       _id: "$ncm",
       stdDev: { $stdDevPop: "$score" },
       scoreMoy: { $avg: "$score" },
       count: { $sum: 1 },
-      items: { $push:  { score: "$score", rna: "$rna",pos:"$pos" }}
+      items: { $push:  { score: "$score", rna: "$rna",pos:"$pos",exp:"$exp" }}
     }
   },
   {
@@ -74,6 +74,41 @@ app.get("/ncm_grouped_Low_std_dev/collection=:collections/kip=:skip/limit=:limit
     });
 });
 
+app.get("/ncm_grouped_Low_std_dev_mcff/collection=:collections/skip=:skip/limit=:limit/countMin=:cmin/stdDevMax=:stdDevMax", function (req, res) {
+  //console.log("id :" + req.params.id)
+
+  db.collection(req.params.collections).aggregate([{ $project: { nts: 1, info:{root:1} } },
+  { $unwind: { path: "$nts" } },
+  { $project: { nts: { $arrayElemAt: ["$nts.ncmTabDG_mcff", 0] }, score: "$nts.score", rna: "$nts.rna_id",pos: "$nts.position",exp:"$info.root" } },
+  { $project: { ncm: "$nts.ncm.merge", score: 1,rna:1,pos:1,exp:1} },
+  {
+    $group: {
+      _id: "$ncm",
+      stdDev: { $stdDevPop: "$score" },
+      scoreMoy: { $avg: "$score" },
+      count: { $sum: 1 },
+      items: { $push:  { score: "$score", rna: "$rna",pos:"$pos",exp:"$exp" }}
+    }
+  },
+  {
+    '$match': {
+      $and: [
+        { _id: { $nin: [/.*L.*/i] } },
+        { 'stdDev': { '$lt': Number(req.params.stdDevMax) } },
+        { 'stdDev': { '$ne': 0 } },
+        { 'count': { '$gt': Number(req.params.cmin) } }]
+    }
+  },
+  { $skip: Number(req.params.skip) },
+  { $limit: Number(req.params.limit) }])
+    .toArray(function (err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contacts.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+});
 
 
 
